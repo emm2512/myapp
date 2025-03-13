111875d56614881f2818c3f59a401c4fe6f9e8f8
emm2512 <emmamound2@gmail.com>
2025-03-13 08:55:04 +0100

    new

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,10 @@
-resource "jenkins-terra" "ec2" {
+resource "aws_instance" "ec2" {
+    
     ami = "ami-04aa00acb1165b32a"
     instance_type = "t2.micro"
-    key_pair = "resume"
+    key_name = "resume"
     availability_zone = "us-east-1a"
-    tags {
-        Name = "myproject"
-        env = "dev"
-        #owner = "qa"
-    }
+    
+
 
 }
